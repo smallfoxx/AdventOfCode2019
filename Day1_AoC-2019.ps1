6f570cba91f019c3fce60ef2970be11d8acf07e6
smallfoxx <38084959+smallfoxx@users.noreply.github.com>
2019-12-01 16:03:30 -0600

    Added Day 1 - Part Two logic
    Added in logic to do recursion for part two of Day 1

--- Day1_AoC-2019.ps1
+++ Day1_AoC-2019.ps1
@@ -4,16 +4,41 @@ Day1_AoC-2019
 
 #>
 [Cmdletbinding()]
-param([parameter(ValueFromPipeline=$true)][int64]$ModuleMass)
+param([parameter(ValueFromPipeline=$true)][int64]$ModuleMass,
+    [switch]$IncludeFuel)
 
 Begin {
+    Function GetFuel {
+        Param([int64]$Mass,
+            [switch]$Recurse)
+
+        If ($Mass -ge 9) {
+            $Fuel = [math]::Floor($Mass/3)-2
+            If ($Recurse) {
+                $Fuel += (GetFuel -Mass $Fuel -Recurse:$Recurse)
+                Write-Host "$Mass`: $Fuel"
+            } 
+            return $Fuel 
+        } else {
+            return 0
+        }
+    }
+
     $TotalFuel = 0
 }
 
 Process {
-    $TotalFuel += [math]::Floor($ModuleMass/3)-2
+    $ThisFuel = (GetFuel -Mass $ModuleMass)
+    If ($IncludeFuel) {
+        $ThisFuel += (GetFuel -Mass $ThisFuel -Recurse)
+    }
+    $TotalFuel += $ThisFuel 
 }
 
 End {
-    return $TotalFuel
+    If ($IncludeFuel) {
+        return ($TotalFuel )#+ (GetFuel -Mass $TotalFuel -Recurse))
+    } else {
+        return $TotalFuel
+    }
 }
